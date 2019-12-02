54964ede565c1747a01d06738cfd9db30f4d31ec
Manu <28782438+manu-p-1@users.noreply.github.com>
2019-12-01 21:48:05 -0500

    New Utilities

--- Executable/Program.cs
+++ Executable/Program.cs
@@ -13,15 +13,6 @@ namespace Note.Executable
     {
         static void Main()
         {
-            //string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Manu\source\repos\Note\Executable\TextFile1.txt");
-            string s = "manu is very cool and he is a cool person";
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            string change = s.Shuffle(true);
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine(elapsedMs);
-            Console.WriteLine(change);
-
         }
     }
 #endif
